1f18b53b7f37cddaae2e2b433719d58f9c755338
BlackCNP <mrx30050@gmail.com>
2023-09-27 12:17:21 +0300

    Lab1
    half of the code

---
+++ Lab1/src/Main.java
@@ -0,0 +1,81 @@
+import java.util.ArrayList;
+
+class Book {
+    private String title;
+    private String author;
+    private String isbn;
+    private int publicationYear;
+
+    public Book(String title, String author, String isbn, int publicationYear) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+        this.publicationYear = publicationYear;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public int getPublicationYear() {
+        return publicationYear;
+    }
+
+    @Override
+    public String toString() {
+        return "Title: " + title + "\nAuthor: " + author + "\nISBN: " + isbn + "\nPublication Year: " + publicationYear + "\n";
+    }
+}
+
+class Library {
+    private ArrayList<Book> books;
+
+    public Library() {
+        books = new ArrayList<>();
+    }
+
+    public void addBook(Book book) {
+        books.add(book);
+    }
+
+    public void displayBooks() {
+        for (Book book : books) {
+            System.out.println(book);
+        }
+    }
+
+    public Book findBookByTitle(String title) {
+        for (Book book : books) {
+            if (book.getTitle().equalsIgnoreCase(title)) {
+                return book;
+            }
+        }
+        return null;
+    }
+
+    public boolean removeBookByISBN(String isbn) {
+        for (Book book : books) {
+            if (book.getIsbn().equals(isbn)) {
+                books.remove(book);
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
+class LibraryManagement {
+    public static void main(String[] args) {
+        Library library = new Library();
+
+
+    }
+}
