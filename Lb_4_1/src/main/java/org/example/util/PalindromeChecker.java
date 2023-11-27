eea1c28c46daea9ccbe2714a162923bd62b29e50
BlackCNP <mrx30050@gmail.com>
2023-11-27 13:28:51 +0200

    Lab_4_1
    write code

---
+++ Lb_4_1/src/main/java/org/example/util/PalindromeChecker.java
@@ -0,0 +1,15 @@
+package org.example.util;
+
+public class PalindromeChecker {
+    public static boolean isPalindrome(String word) {
+        int i = 0, j = word.length() - 1;
+
+        while (i < j) {
+            if (word.charAt(i) != word.charAt(j))
+                return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
